53bba2e5e6593200f4c39c80a5161306a5dfdd64
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-09 00:01:43 +0200

    file structure , add and get posts

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+import postRoute from "./routes/posts.js"
+
 const app = express();
+
+app.use("/posts", postRoute)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 8090
+const PORT = process.env.PORT || 8090;
 
 mongoose.connect(process.env.DATABASE_URL)
     .then(() => app.listen(PORT, () => console.log(`listening port ${PORT}`)))
