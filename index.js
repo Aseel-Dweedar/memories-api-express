d6edc3f8fd48fbb78863ee23568e5ac8971495dd
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-08 22:59:18 +0200

    server setyp files

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 8090
+
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => app.listen(PORT, () => console.log(`listening port ${PORT}`)))
+    .catch(err => console.log(err.message));
