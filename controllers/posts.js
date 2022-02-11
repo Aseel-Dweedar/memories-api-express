a08f00980c43b823fedeb9543cf54de2c8fe1240
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-11 21:11:41 +0200

    update post ipa

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,12 +11,37 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
 
-    const post = req.body.post;
+    const post = req.body;
     const newPost = new PostMessage(post);
 
     try {
         await newPost.save();
         res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+
+    // re-name id to _id
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json("No Post With This Id!");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    res.json(updatedPost);
+
+}
+
+export const deletePost = async (req, res) => {
+
+    const { id: _id } = req.params;
+
+    try {
+        await PostMessage.deleteOne({ _id });
+        res.status(202).json("Deleted successfully")
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
