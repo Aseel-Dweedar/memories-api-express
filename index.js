f39e46b68f039c87c2d91d90c1cc5dd1e8f72a77
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-13 20:44:39 +0200

    jwt login , jwt auth , auth middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import postRoute from "./routes/posts.js"
+import userRoute from "./routes/users.js"
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoute);
+app.use("/user", userRoute);
 
 app.get('/', (req, res) => res.send("YOLO !!"));
 
