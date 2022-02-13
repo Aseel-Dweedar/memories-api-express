9542d12317836733e9b04632eabd12de206ff774
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-13 20:57:10 +0200

    update likes by user

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,10 +40,22 @@ export const likePost = async (req, res) => {
 
     const { id } = req.params;
 
+    if (!req.userId) return res.json({ message: 'Unauthenticated' })
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json("No Post With This Id!");
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes.filter((id) => id != req.userId)
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
     res.json(updatedPost);
 
 }
