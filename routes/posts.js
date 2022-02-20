7f0e70530972ce2d68de9aeddcd7c5b402d628ee
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-20 22:44:47 +0200

    fetch one post

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
 import auth from '../middleware/auth.js'
 
-import { getPosts, getPostsBySearch, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js"
+import { getPosts, getPost, getPostsBySearch, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js"
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.get("/search", getPostsBySearch);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
